4224b54359e22f923ad3b5c0bd1d4f20e69d1f03
lilyengerage12-ux <lilyengerage12@gmail.com>
2026-07-18 09:30:44 +0800

    chore: add Supabase client configuration template

---
+++ config.example.js
@@ -0,0 +1,5 @@
+// Copy to config.js after creating a Supabase project.
+window.CAROV_CLOUD = {
+  supabaseUrl: "https://YOUR_PROJECT.supabase.co",
+  publishableKey: "sb_publishable_REPLACE_ME"
+};
